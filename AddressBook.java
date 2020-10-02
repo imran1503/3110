d4899959d0cebade66c8e381f5c10bdf3075b726
Imran Latif <wolf2184@gmail.com>
2020-10-01 22:25:11 -0400

    -Fixed RemoveBuddy() -Fixed Main()

--- AddressBook.java
+++ AddressBook.java
@@ -8,21 +8,22 @@ public class AddressBook {
     }
 
     public void addBuddy(BuddyInfo buddy){
-        if(buddy != null){
+        if(buddy != null ){
             myBuddies.add(buddy);
         }
     }
 
     public void removeBuddy(int index){
-        if(index >=0 && index < myBuddies.size()){
-            myBuddies.remove();
-        }
+       if(index >=0 && index < myBuddies.size()){
+           myBuddies.remove(index);
+       }
     }
 
-    public void main(){
+    public static void main(String[] args){
         BuddyInfo buddy  = new BuddyInfo("Tom", "Carleton",613 );
         System.out.println( "Address Book");
-        AddressBook addressBook = new AddressBook(null, null, 0);
+        AddressBook addressBook = new AddressBook();
+        //addressBook
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(0);
 
